8ae4fa1bd2c45cc56181a9ad37d57efe4eb03280
miguel de la peña <xtremzmiguel@gmail.com>
2019-12-05 11:05:03 +0100

    metodos

--- src/ejercicos/metodos.java
+++ src/ejercicos/metodos.java
@@ -1,5 +1,9 @@
 package ejercicos;
 
 public class metodos {
-
+	public static void main (String[] args) {
+		String var1 = new String("hola");
+		String var2 = new String("hola");
+		String concat1 = var2.concat(var1);
+}
 }
