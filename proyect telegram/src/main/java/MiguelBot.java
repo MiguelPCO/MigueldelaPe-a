309c47dd80f5e872c7e74146607b1bcaaacf2db6
MiguelPCO <58552233+MiguelPCO@users.noreply.github.com>
2020-04-30 13:56:10 +0200

    Add files via upload

---
+++ proyect telegram/src/main/java/MiguelBot.java
@@ -0,0 +1,16 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MiguelBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return "migueloide69_bot";
+    }
+
+    public String getBotToken() {
+        return "1110612112:AAEiPpvMCmmEYkL8bGQ5EbBbegAuRt7uc9g";
+    }
+}
